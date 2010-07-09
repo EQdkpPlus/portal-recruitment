95bffaa7ad2ee577108e7709f75feb4cd61a38a8
corgan <corgan@eqdkp-plus.eu>
2010-07-09 16:07:05 +0000

    french & spanish translation

--- language/french.php
+++ language/french.php
@@ -1,30 +1,19 @@
 <?php
-/*
- * Project:     EQdkp-Plus
- * License:     Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
- * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
- * -----------------------------------------------------------------------
- * Began:       2008
- * Date:        $Date: 2008-09-04 21:05:57 +0200 (Do, 04 Sep 2008) $
- * -----------------------------------------------------------------------
- * @author      $Author: wallenium $
- * @copyright   2006-2008 Corgan - Stefan Knaak | Wallenium & the EQdkp-Plus Developer Team
- * @link        http://eqdkp-plus.com
- * @package     eqdkp-plus
- * @version     $Rev: 2676 $
- * 
- * $Id: english.php 2676 2008-09-04 19:05:57Z wallenium $
- */
 
-
-if ( !defined('EQDKP_INC') ){
-    header('HTTP/1.0 404 Not Found');exit;
+if (!defined('EQDKP_INC')) {
+	die('You cannot access this file directly.');
 }
 
-$plang = array_merge($plang, array(
-  'recruitment'             => 'Recrutement',
-  'recruitment_open'        => 'Recrutement ouvert',
-  'recruitment_contact'     => 'Postuler',
-  'recruitment_noneed'      => 'Le recrutement est actuellement fermé.',
-));
+//Language: French	
+//Created by EQdkp Plus Translation Tool on  2010-07-09 13:55
+//File: module_recruitment
+//Source-Language: english
+
+$alang = array( 
+"recruitment" => "Recrutement",
+"recruitment_open" => "Recrutement ouvert",
+"recruitment_contact" => "Postuler",
+"recruitment_noneed" => "Le recrutement est actuellement fermé.",
+ );
+$plang = array_merge($plang, $alang);
 ?>
---
+++ language/spanish.php
@@ -0,0 +1,19 @@
+<?php
+
+if (!defined('EQDKP_INC')) {
+	die('You cannot access this file directly.');
+}
+
+//Language: Spanish	
+//Created by EQdkp Plus Translation Tool on  2010-07-09 15:10
+//File: module_recruitment
+//Source-Language: english
+
+$alang = array( 
+"recruitment" => "Reclutamiento",
+"recruitment_open" => "Buscando miembros",
+"recruitment_contact" => "Solicitud",
+"recruitment_noneed" => "No hay huecos disponibles por el momento.",
+ );
+$plang = array_merge($plang, $alang);
+?>
