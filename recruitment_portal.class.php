8a561a2d561f0f51d5dc3a78a936a0460f047ebb
GodMod <GodMod@users.noreply.github.com>
2015-05-04 11:22:35 +0200

    Update recruitment_portal.class.php

--- recruitment_portal.class.php
+++ recruitment_portal.class.php
@@ -32,7 +32,7 @@ class recruitment_portal extends portal_generic {
 	protected static $path		= 'recruitment';
 	protected static $data		= array(
 		'name'			=> 'Recruitment Module',
-		'version'		=> '2.0.3',
+		'version'		=> '2.0.4',
 		'author'		=> 'GodMod',
 		'icon'			=> 'fa-search-plus',
 		'contact'		=> EQDKP_PROJECT_URL,
